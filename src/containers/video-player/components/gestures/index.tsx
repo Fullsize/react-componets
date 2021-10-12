a85a531a95f5276d5e3fc5e6acc329d9f7d79651
无涯 <1619686166@qq.com>
2021-10-12 14:51:46 +0800

    feat: optimize

--- src/containers/video-player/components/gestures/index.tsx
+++ src/containers/video-player/components/gestures/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Fullsize
  * @Date: 2021-10-11 15:27:37
  * @LastEditors: Fullsize
- * @LastEditTime: 2021-10-12 11:54:00
+ * @LastEditTime: 2021-10-12 14:44:04
  * @FilePath: /react-context/src/containers/video-player/components/gestures/index.tsx
  */
 import React, { useRef, useEffect, useContext, useState, useCallback } from "react";
@@ -23,18 +23,18 @@ const getTrackTranslateX = ({ duration, currentTime }: any) => {
 const updateValue = (value: number, oldstep: number, newstep: number, positive: boolean) => {
 	const num = Math.abs(Math.abs(oldstep) - Math.abs(newstep)) / 5
 	const v = (positive ? num : - num)
-	console.log(29,num)
+	console.log(29, num)
 	return v || 0
 }
 const Gestures: React.FC = () => {
 	const oldPosition: any = useRef(null);
 	const player = useContext(PlayContext);
-	const { states: { duration, currentTime }, controls: { changeCurrentTime, pause },dispatch} = player;
+	const { states: { duration, currentTime }, controls: { changeCurrentTime, pause }, dispatch } = player;
 	const gestureRef = useRef(null);
 	const [value, setValue] = useState(0);
 	const [siding, setSiding] = useState(true)
 	const [mounted, setMounted] = useState(false);
-	const [showTime,setShowTime]=useState(false);
+	const [showTime, setShowTime] = useState(false);
 	// 开始
 	const handPanStart = useCallback((e: any) => {
 		e.preventDefault();
@@ -42,16 +42,18 @@ const Gestures: React.FC = () => {
 		setSiding(false)
 		setShowTime(true)
 		setValue(0)
-		dispatch({type:'custom',custom:{
-			isMotion:true
-		}})
+		dispatch({
+			type: 'custom', custom: {
+				isMotion: true
+			}
+		})
 		oldPosition.current = { x: e.deltaX, y: e.deltaY, type: e.type }
 	}, [dispatch, pause])
 	// 右
 	const handPanRight = useCallback((e: any) => {
 		e.preventDefault();
-		const { x ,type} = oldPosition.current;
-		if(type==='panleft'){
+		const { x, type } = oldPosition.current;
+		if (type === 'panleft') {
 			setSiding(true)
 			setValue(0);
 			return false;
@@ -60,14 +62,14 @@ const Gestures: React.FC = () => {
 		console.log(duration)
 		setValue(v)
 		oldPosition.current = { ...oldPosition.current, type: e.type }
-		
+
 	}, [duration, value])
 	// 左滑
 	const handPanLeft = useCallback((e: any) => {
 		e.preventDefault();
-	
-		const { x,type } = oldPosition.current;
-		if(type==='panright'){
+
+		const { x, type } = oldPosition.current;
+		if (type === 'panright') {
 			setSiding(true)
 			setValue(0);
 			return false;
@@ -80,14 +82,16 @@ const Gestures: React.FC = () => {
 	const handPanEnd = useCallback(() => {
 		setSiding(true)
 		setValue(0);
-		
+		console.log(83, '已经结束')
 	}, [])
-	const hideSidTime=_.debounce(()=>{
+	const hideSidTime = _.debounce(() => {
 		setShowTime(false)
-		dispatch({type:'custom',custom:{
-			isMotion:false
-		}})
-	},0)
+		dispatch({
+			type: 'custom', custom: {
+				isMotion: false
+			}
+		})
+	}, 0)
 	useEffect(() => {
 		if (siding && mounted) {
 			changeCurrentTime(currentValue >= duration ? duration : currentValue)
@@ -101,10 +105,14 @@ const Gestures: React.FC = () => {
 		if (!mounted && gestureRef.current) {
 			let hammer: any = null;
 			hammer = new Hammer(gestureRef.current);
+			// hammer.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 			hammer.on('panstart', (e: any) => handPanStart(e))
 			hammer.on('panright', (e: any) => handPanRight(e))
 			hammer.on('panleft', (e: any) => handPanLeft(e))
+			hammer.on('panup', handPanEnd)
+			hammer.on('pandown', handPanEnd)
 			hammer.on('panend', (e: any) => handPanEnd())
+
 			// hammer.on('pancancel', (e: any) => handPanEnd())
 			setMounted(true)
 		}
