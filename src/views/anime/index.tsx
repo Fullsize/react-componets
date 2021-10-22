2b51676b40bbd81193c1f4554bd022eef86077a2
无涯 <1619686166@qq.com>
2021-10-22 11:41:17 +0800

    feat: anime

--- src/views/anime/index.tsx
+++ src/views/anime/index.tsx
@@ -1,23 +1,15 @@
 /*
  * @Author: Fullsize
- * @Date: 2021-10-21 16:36:11
+ * @Date: 2021-10-22 10:20:06
  * @LastEditors: Fullsize
- * @LastEditTime: 2021-10-22 11:04:48
+ * @LastEditTime: 2021-10-22 11:40:43
  * @FilePath: /react-context/src/views/anime/index.tsx
  */
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import bg from './img/bg.jpeg'
-import { Swiper, SwiperSlide } from "swiper/react";
 import cover from './img/cover.jpeg'
 import styles from './index.module.css';
-import SwiperCore, {
-	EffectCoverflow, Pagination
-} from 'swiper';
-
-import "swiper/css";
-import "swiper/css/effect-coverflow"
-import "swiper/css/pagination"
-SwiperCore.use([EffectCoverflow, Pagination]);
+declare const window: any;
 const Anime: React.FC = () => {
 	const handleMouseEnter = useCallback((e) => {
 		e.target.play();
@@ -25,6 +17,25 @@ const Anime: React.FC = () => {
 	const handleMouseLeave = useCallback((e) => {
 		e.target.pause();
 		e.target.currentTime = 0;
+	}, [])
+	useEffect(() => {
+		new window.Swiper(".mySwiper", {
+			effect: "coverflow",
+			grabCursor: true,
+			centeredSlides: true,
+			slidesPerView: "auto",
+			coverflowEffect: {
+				rotate: 50,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: true,
+			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+		});
 	}, [])
 	return (
 		<>
@@ -55,7 +66,7 @@ const Anime: React.FC = () => {
 				<div className={styles["cover"]}>
 					<img className={styles["img"]} src={cover} alt="" />
 					<div className={styles["context"]}>
-						<h2 className={styles['title']}>大桥</h2>
+						<h2>大桥</h2>
 					</div>
 				</div>
 				<div className={styles["cover"]}>
@@ -65,24 +76,39 @@ const Anime: React.FC = () => {
 					</div>
 				</div>
 			</div>
-			<div className={styles['main']}>
-				<Swiper effect={'coverflow'} grabCursor={true} centeredSlides={true} slidesPerView={'auto'} coverflowEffect={{
-					"rotate": 50,
-					"stretch": 0,
-					"depth": 100,
-					"modifier": 1,
-					"slideShadows": true
-				}} pagination={true} className="mySwiper">
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" /></SwiperSlide>
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" /></SwiperSlide>
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" /></SwiperSlide>
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" /></SwiperSlide>
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="" /></SwiperSlide>
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="" /></SwiperSlide>
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="" /></SwiperSlide>
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="" /></SwiperSlide>
-					<SwiperSlide><img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="" /></SwiperSlide>
-				</Swiper>
+			<div className={styles["main"]} style={{ 'maxWidth': '500px', 'overflow': 'hidden', 'margin': '0 auto' }}>
+				<div className="swiper mySwiper">
+					<div className="swiper-wrapper">
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+						<div className="swiper-slide">
+							<img src={bg} alt="" />
+						</div>
+					</div>
+					<div className="swiper-pagination"></div>
+				</div>
 			</div>
 		</>
 	)
